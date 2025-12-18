69c440951e8fdc33982e5f871f1f77589953ee89
fmascena-dev <felipe.mascena.dev@gmail.com>
2025-12-17 21:07:42 -0300

    feat: add lib ui

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import { TooltipProvider } from "@/components/ui/tooltip"
+import Sonner from "./components/ui/sonner/Sonner.vue";
+</script>
+
+<template>
+  <TooltipProvider>
+    <Sonner />
+    <RouterView />
+  </TooltipProvider>
+</template>
