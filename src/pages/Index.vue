2500452c5edd6fa34dc22fbd2dabeb35a3a54d12
fmascena-dev <felipe.mascena.dev@gmail.com>
2025-12-17 23:53:59 -0300

    feat: add component page

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -2,10 +2,12 @@
 <Snowfall />
 <HeroSection />
 <PhotoCarousel />
+<VideoSection />
 </template>
 
 <script setup lang="ts">
 import HeroSection from '@/components/HeroSection.vue';
 import PhotoCarousel from '@/components/PhotoCarousel.vue';
 import Snowfall from '@/components/Snowfall.vue';
+import VideoSection from '@/components/VideoSection.vue';
 </script>
