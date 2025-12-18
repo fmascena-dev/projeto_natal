e37a952a0490c41928bd303c169141b3b0e5f280
fmascena-dev <felipe.mascena.dev@gmail.com>
2025-12-17 21:09:41 -0300

    feat: add route page

---
+++ src/pages/Index.vue
@@ -0,0 +1,11 @@
+<template>
+<Snowfall />
+<HeroSection />
+<PhotoCarousel />
+</template>
+
+<script setup lang="ts">
+import HeroSection from '@/components/HeroSection.vue';
+import PhotoCarousel from '@/components/PhotoCarousel.vue';
+import Snowfall from '@/components/Snowfall.vue';
+</script>
