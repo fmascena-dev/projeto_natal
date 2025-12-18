ee3a5c6af40c799eed6393fe87daafa674e5b226
fmascena-dev <felipe.mascena.dev@gmail.com>
2025-12-18 01:07:14 -0300

    fix: add new pages components

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,13 +1,17 @@
 <template>
-<Snowfall />
-<HeroSection />
-<PhotoCarousel />
-<VideoSection />
+  <Snowfall />
+  <HeroSection />
+  <PhotoCarousel />
+  <VideoSection />
+  <CardCreator />
+  <Footer />
 </template>
 
 <script setup lang="ts">
-import HeroSection from '@/components/HeroSection.vue';
-import PhotoCarousel from '@/components/PhotoCarousel.vue';
-import Snowfall from '@/components/Snowfall.vue';
-import VideoSection from '@/components/VideoSection.vue';
+import CardCreator from '@/components/CardCreator.vue';
+import Footer from '@/components/Footer.vue';
+import HeroSection from '@/components/HeroSection.vue'
+import PhotoCarousel from '@/components/PhotoCarousel.vue'
+import Snowfall from '@/components/Snowfall.vue'
+import VideoSection from '@/components/VideoSection.vue'
 </script>
